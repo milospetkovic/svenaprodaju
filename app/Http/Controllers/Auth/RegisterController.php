da309d043a09a25ab893131ebaec742e266e2e7e
petkovicmilos <mail2milos@yahoo.com>
2018-01-31 20:01:57 +0100

    fixes for sql calling, adjusting variables for new dev environment

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -113,7 +113,7 @@ class RegisterController extends Controller
 
         //Flash::message('Thanks for signing up! Please check your email.');
 
-        return Redirect::home();
+//        return Redirect::home();
 
 
 //        $this->guard()->login($user);
