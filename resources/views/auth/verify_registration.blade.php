219aa631f9bdf93004cf2b78407f9e6362f7c21c
petkovicmilos <mail2milos@yahoo.com>
2018-02-04 17:03:40 +0100

    update

---
+++ resources/views/auth/verify_registration.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+
+            {{ $confirmation_code }}  ovde za verifikaciju
+
+        </div>
+    </div>
+@endsection
