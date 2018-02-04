80d83f3e331c55b81575c3dae1a4ecba15d84751
petkovicmilos <mail2milos@yahoo.com>
2018-02-04 22:42:54 +0100

    work with laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,10 @@ let mix = require('laravel-mix');
 
 mix.js(['resources/assets/js/app.js',
         'resources/assets/js/bootstrap.js',
-        'resources/assets/js/elitasoft.js'],
-    'public/js');
-mix.sass('resources/assets/sass/app.scss', 'public/css');
+        'resources/assets/js/elitasoft.js,',
+        'resources/assets/vendor/inspinia/js/plugins/toastr/toastr.min.js'],
+    'public/js/all.js');
+
+mix.styles(['resources/assets/vendor/inspinia/css/plugins/toastr/toastr.min.css'],
+        'public/css/all.css');
+mix.sass('resources/assets/sass/app.scss', 'public/css/all.scss');
