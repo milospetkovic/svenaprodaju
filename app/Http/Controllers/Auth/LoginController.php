b4b84d54125727a7bde335aebfff5836e400bd2d
petkovicmilos <mail2milos@yahoo.com>
2018-02-06 22:26:19 +0100

    faefae

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -103,7 +103,7 @@ class LoginController extends Controller
         $this->clearLoginAttempts($request);
 
         return $this->authenticated($request, $this->guard()->user())
-            ?: redirect()->intended($this->redirectPath());
+            ?: redirect()->intended($this->redirectPath()); 
     }
 
 }
