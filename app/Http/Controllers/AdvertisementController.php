6e1d3eeae85ea3da35fc379448b04fbd88032805
petkovicmilos <mail2milos@yahoo.com>
2018-02-10 17:15:25 +0100

    redirect to login page if user isn't logged in when trying to create advertisement

--- app/Http/Controllers/AdvertisementController.php
+++ app/Http/Controllers/AdvertisementController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Auth\Access\Gate;
+use App\User;
 
 class AdvertisementController extends Controller
 {
-    public function viewCreateForm(Request $request)
+    public function viewCreateForm(Request $request, User $user)
     {
         //$this->authorize('viewcreateform', 'AdvertisementPolicy');
-        if ((new Gate)->allows('view-create', 'AdvertisementPolicy')) {
-
+        if (auth()->user()) {
             return view('advertisement.create');
+        } else {
+            return view('auth.login');
         }
     }
 }
