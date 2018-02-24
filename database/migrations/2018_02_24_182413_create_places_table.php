75a2576e8a85f9a11abc555c371ff66089ca4ca8
petkovicmilos <mail2milos@yahoo.com>
2018-02-24 19:32:09 +0100

    migration for places table (all places with postcodes in Serbia)

---
+++ database/migrations/2018_02_24_182413_create_places_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_code')->nullable(false);
+            $table->string('place_name')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+}
