3080863dc2cc64dd8471e37b9fd89be4662e330a
petkovicmilos <mail2milos@yahoo.com>
2018-02-24 22:37:38 +0100

    wip - view for create advertisement action

--- resources/views/advertisement/create.blade.php
+++ resources/views/advertisement/create.blade.php
@@ -6,26 +6,157 @@
         <div class="tabs-container col-xs-12">
 
             <div>
-                Formular za unos oglasa
+                <h2>Formular za unos oglasa</h2>
             </div>
 
             <div class="panel-body">
+
                 <form action="{{ action('AdvertisementController@viewCreateForm') }}" method="post" class="form-horizontal">
 
                     {{ csrf_field() }}
 
-                    <div class="">
-                        <label for="title">Naziv oglasa</label>
-                        <input placeholder="npr. Laptop ili Kosilica 4x4..." type="text" name="title" id="title">
-                    </div>
-                    <div class="">
-                        <label for="description">Opis oglasa</label>
-                        <textarea id="description" name="description"></textarea>
-                    </div>
-                    <div class="">
-                        <button type="submit" class="btn btn-primary m-r"><span class="fa fa-check"></span> @lang('Sačuvaj')</button>
-                    </div>
+                    <table class="table table-hover">
+
+                        <tbody>
+
+                            <tr>
+                                <td class="text-right">
+                                    <label for="title">Naziv oglasa*: </label>
+                                </td>
+                                <td>
+                                    <input placeholder="npr. Laptop ili Kosilica 4x4..." type="text" name="title" id="title">
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-right">
+                                    <label for="adv_typ">Tip oglasa*: </label>
+                                </td>
+                                <td>
+                                    <label>
+                                        <input type="radio" name="fk_type" id="fk_type" value="1" checked> Prodajem
+                                    </label>
+
+                                    <label style="margin-left: 10px;">
+                                        <input type="radio" name="fk_type" id="fk_type" value="2"> Kupujem
+                                    </label>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-right">
+                                    <label for="price">Cena*: </label>
+                                </td>
+                                <td>
+                                    <label>
+                                        <input type="text" name="price" id="price">
+                                    </label>
+
+                                    <label title="RSD - Republika Srbija Dinar" style="margin-left: 10px;">
+                                        <input type="radio" name="fk_price_currency" id="fk_price_currency" value="RSD" checked> Din
+                                    </label>
+
+                                    <label title="EURO" style="margin-left: 10px;">
+                                        <input type="radio" name="fk_price_currency" id="fk_price_currency" value="EUR"> <span class="glyphicon-class glyphicon glyphicon-euro"></span>
+                                    </label>
+
+                                    <div>
+                                        <label>
+                                            <input type="checkbox" name="accept_replacement" value="1" id="accept_replacement"> Prihvatam zamenu
+                                        </label>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            {{--
+                            <tr>
+                                <td class="text-right">
+                                    <label for="price">Prihvatam zamenu: </label>
+                                </td>
+                                <td>
+                                    <label>
+                                        <input type="checkbox" name="accept_replacement" value="1" id="accept_replacement">
+                                    </label>
+                                </td>
+                            </tr>
+                            --}}
+
+                            <tr>
+                                <td class="text-right">
+                                    <label for="description">Stanje*: </label>
+                                </td>
+                                <td>
+                                    <label>
+                                        <input type="radio" name="fk_condition" id="fk_condition" value="1" checked> Polovno (korišćeno)
+                                    </label>
+
+                                    <label style="margin-left: 10px;">
+                                        <input type="radio" name="fk_condition" id="fk_condition" value="2"> Novo
+                                    </label>
+
+                                    <label style="margin-left: 10px;">
+                                        <input type="radio" name="fk_condition" id="fk_condition" value="3"> Neispravno (oštećeno)
+                                    </label>
+
+                                    <label style="margin-left: 10px;">
+                                        <input type="radio" name="fk_condition" id="fk_condition" value="4"> Nedefinisano
+                                    </label>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-right">
+                                    <label for="description">Opis oglasa*: </label>
+                                </td>
+                                <td>
+                                    <textarea id="description" name="description"></textarea>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-right">
+                                    <label for="description">Kategorija: </label>
+                                </td>
+                                <td>
+                                    <select class="form-control">
+                                        <option></option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                    </select>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-right">
+                                    <label for="description">Grupa: </label>
+                                </td>
+                                <td>
+                                    <select class="form-control">
+                                        <option></option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                    </select>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td class="text-right">
+                                </td>
+                                <td>
+                                    <div class="">
+                                        <button type="submit" class="btn btn-primary m-r"><span class="fa fa-check"></span> @lang('Sačuvaj')</button>
+                                    </div>
+                                </td>
+                            </tr>
+
+                        </tbody>
+
+                    </table>
+
                 </form>
+
             </div>
 
         </div>
